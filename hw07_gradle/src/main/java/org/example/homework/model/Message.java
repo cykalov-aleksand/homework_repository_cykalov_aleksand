2f3d02634c25d8e0a81a4142ad367ddeefc3e540
1в <cykalov.aleksand@mail.ru>
2026-08-15 22:43:29 +0300

    Message

--- hw07_gradle/src/main/java/org/example/homework/model/Message.java
+++ hw07_gradle/src/main/java/org/example/homework/model/Message.java
@@ -14,6 +14,9 @@ public class Message {
     private final String field8;
     private final String field9;
     private final String field10;
+    private final String field11;
+    private final String field12;
+    private final ObjectForMessage field13;
 
     // todo: 1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
 
@@ -28,7 +31,10 @@ public class Message {
             String field7,
             String field8,
             String field9,
-            String field10) {
+            String field10,
+            String field11,
+            String field12,
+            ObjectForMessage field13) {
         this.id = id;
         this.field1 = field1;
         this.field2 = field2;
@@ -40,6 +46,9 @@ public class Message {
         this.field8 = field8;
         this.field9 = field9;
         this.field10 = field10;
+        this.field11=field11;
+        this.field12=field12;
+        this.field13=field13;
     }
 
     public long getId() {
@@ -85,7 +94,17 @@ public class Message {
     public String getField10() {
         return field10;
     }
+    public String getField11() {
+        return field11;
+    }
 
+    public String getField12() {
+        return field12;
+    }
+
+    public ObjectForMessage getField13() {
+        return field13;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -102,7 +121,7 @@ public class Message {
     }
 
     public Builder toBuilder() {
-        return new Builder(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10);
+        return new Builder(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10,field11, field12, field13);
     }
 
     @Override
@@ -118,7 +137,9 @@ public class Message {
                 + field7 + '\'' + ", field8='"
                 + field8 + '\'' + ", field9='"
                 + field9 + '\'' + ", field10='"
-                + field10 + '\'' + '}';
+                + field10 + '\'' + ", field11='"
+                + field11 + '\'' + ", field12='"
+                + field13 + '\'' + '}';
     }
 
     public static class Builder {
@@ -133,7 +154,9 @@ public class Message {
         private String field8;
         private String field9;
         private String field10;
-
+        private String field11;
+        private String field12;
+        private ObjectForMessage field13;
         public Builder(long id) {
             this.id = id;
         }
@@ -149,7 +172,10 @@ public class Message {
                 String field7,
                 String field8,
                 String field9,
-                String field10) {
+                String field10,
+                String field11,
+                String field12,
+                ObjectForMessage field13) {
             this.id = id;
             this.field1 = field1;
             this.field2 = field2;
@@ -161,6 +187,9 @@ public class Message {
             this.field8 = field8;
             this.field9 = field9;
             this.field10 = field10;
+            this.field11 = field11;
+            this.field12 = field12;
+            this.field13 = field13;
         }
 
         public Builder field1(String field1) {
@@ -212,9 +241,23 @@ public class Message {
             this.field10 = field10;
             return this;
         }
+        public Builder field11(String field11) {
+            this.field11 = field11;
+            return this;
+        }
+
+        public Builder field12(String field12) {
+            this.field12 = field12;
+            return this;
+        }
+
+        public Builder field13(ObjectForMessage field13) {
+            this.field13 = field13;
+            return this;
+        }
 
         public Message build() {
-            return new Message(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10);
+            return new Message(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10,field11, field12, field13);
         }
     }
 }
