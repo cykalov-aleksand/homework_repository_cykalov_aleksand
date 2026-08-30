debc8a62151826dbd80876ef9b503a7a59348d31
1в <cykalov.aleksand@mail.ru>
2026-08-30 08:33:27 +0300

    Commit initialization

--- hw08_gradle/src/main/java/org/example/dataprocessor/ProcessorAggregator.java
+++ hw08_gradle/src/main/java/org/example/dataprocessor/ProcessorAggregator.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 public class ProcessorAggregator implements Processor {
 
+
+
     @Override
     public Map<String, Double> process(List<Measurement> data) {
         // группирует выходящий список по name, при этом суммирует поля value
