5303747ae627570ab9728a10fd0905e4e73e6bb7
1в <cykalov.aleksand@mail.ru>
2026-09-12 20:21:07 +0300

    тесты

--- hw09_gradle/src/test/java/org/example/repository/executor/DbExecutorImplTest.java
+++ hw09_gradle/src/test/java/org/example/repository/executor/DbExecutorImplTest.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
